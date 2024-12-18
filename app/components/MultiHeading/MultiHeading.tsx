660c137ee3a0aefd5ec483989a175e19874ae884
Muhammad Subhan <subhanghani339@gmail.com>
2024-12-19 03:02:42 +0500

    font size decreased

--- app/components/MultiHeading/MultiHeading.tsx
+++ app/components/MultiHeading/MultiHeading.tsx
@@ -14,14 +14,14 @@ const MultiHeading: React.FC<MultiHeadingProps> = ({
   return (
     <div>
       <h2
-        className={`special-text text-3xl mb-3 text-brand-500 ${
+        className={`special-text text-xl md:text-3xl mb-3 text-brand-500 ${
           textAlignment ? textAlignment : "text-left"
         }`}
       >
         {subheading}
       </h2>
       <h3
-        className={`text-5xl text-white font-bold ${
+        className={`text-3xl md:text-5xl text-white font-bold ${
           textAlignment ? textAlignment : "text-left"
         }`}
       >
