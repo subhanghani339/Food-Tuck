12fe5f5255bea5b7fcadbd6a44dc4020d6ad65f6
Muhammad Subhan <subhanghani339@gmail.com>
2025-01-07 20:04:57 +0500

    Fur added in testimonials

--- app/components/Testimonials/Testimonials.tsx
+++ app/components/Testimonials/Testimonials.tsx
@@ -7,6 +7,7 @@ import TestimonialCard from "./TestimonialCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import Image from "next/image";
 
 const Testimonials = ({ hasBoxShadow = false, changeHeadingColor = false }) => {
   var settings = {
@@ -26,19 +27,30 @@ const Testimonials = ({ hasBoxShadow = false, changeHeadingColor = false }) => {
         singleColorHeading={changeHeadingColor}
       />
 
-      <div className="max-w-4xl mx-auto my-14">
-        <Slider {...settings}>
-          {clientFeedbacks.map((feedback, index) => (
-            <TestimonialCard
-              key={index}
-              image={feedback.image}
-              name={feedback.name}
-              feedback={feedback.feedback}
-              designation={feedback.designation}
-              hasBoxShadow={hasBoxShadow}
-            />
-          ))}
-        </Slider>
+      <div className="max-w-5xl mx-auto relative">
+        <div className="max-w-4xl my-14">
+          <Slider {...settings}>
+            {clientFeedbacks.map((feedback, index) => (
+              <TestimonialCard
+                key={index}
+                image={feedback.image}
+                name={feedback.name}
+                feedback={feedback.feedback}
+                designation={feedback.designation}
+                hasBoxShadow={hasBoxShadow}
+              />
+            ))}
+          </Slider>
+        </div>
+        <div className="absolute top-8 right-3">
+          <Image
+            src="/images/home/fur.svg"
+            alt="fur"
+            width={396}
+            height={500}
+            priority
+          />
+        </div>
       </div>
     </div>
   );
