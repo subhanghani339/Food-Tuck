8513d54625e280319c250cde5b87d56144a71d75
subhan-skysys <muhammad.subhan@myskysys.com>
2025-01-16 23:26:58 +0500

    Blog updated

--- app/components/AllBlogs/AllBlogs.tsx
+++ app/components/AllBlogs/AllBlogs.tsx
@@ -1,6 +1,7 @@
-import { allBlogs } from '@/app/data/dummyData'
-import Image from 'next/image'
 import React from 'react'
+import Image from 'next/image'
+import { allBlogs } from '@/app/data/dummyData'
+import { FaRegCalendarAlt, FaRegCommentDots, FaUserAlt } from 'react-icons/fa';
 
 const AllBlogs = () => {
     return (
@@ -17,6 +18,20 @@ const AllBlogs = () => {
                                 priority
                             />
                             <div className='max-w-3xl'>
+                            <div className="flex items-center text-sm text-gray-500 space-x-4 my-2">
+                                    <div className="flex items-center">
+                                        <FaRegCalendarAlt className="mr-1" />
+                                        <span>{blog.date}</span>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <FaRegCommentDots className="mr-1" />
+                                        <span>{blog.comments} Comments</span>
+                                    </div>
+                                    <div className="flex items-center">
+                                        <FaUserAlt className="mr-1" />
+                                        <span>{blog.user}</span>
+                                    </div>
+                                </div>
                                 <h3 className='text-grey-100 text-xl font-bold'>{blog.title}</h3>
                                 <hr className='my-6' />
                                 <p>{blog.shortDescription}</p>
