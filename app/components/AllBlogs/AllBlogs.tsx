10bcefc38e76eb1a2a0b59f6cd25ca806d58cdf6
Muhammad Subhan <subhanghani339@gmail.com>
2025-01-18 04:24:19 +0500

    margin and padding updated

--- app/components/AllBlogs/AllBlogs.tsx
+++ app/components/AllBlogs/AllBlogs.tsx
@@ -12,8 +12,8 @@ const AllBlogs: React.FC = () => {
   }, []);
 
   return (
-    <div className="container py-10 md:py-16">
-      <div className="grid grid-cols-12 gap-6 mt-5">
+    <div className="container py-8 lg:py-16">
+      <div className="grid grid-cols-12 gap-6 mt-0 lg:mt-5">
         <div className="col-span-12 lg:col-span-9 order-2 lg:order-1">
           {allBlogs.map((blog) => (
             <BlogPost key={blog.slug} blog={blog} />
