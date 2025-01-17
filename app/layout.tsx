fc85e61d5dd33519d8bfb3fa89018c1ad4132aec
Muhammad Subhan <subhanghani339@gmail.com>
2025-01-18 04:35:44 +0500

    Layout File Updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter, Great_Vibes } from "next/font/google";
+import { Inter, Great_Vibes } from "next/font/google";
 import "./globals.css";
 import SessionProviderWrapper from "./components/SessionProvider/SessionProviderWrapper";
 
