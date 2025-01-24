858acd1daf5a760f7dca9dd4a2a1180255b28802
Muhammad Subhan <subhanghani339@gmail.com>
2025-01-25 04:59:35 +0500

    Login Register Updated

--- app/components/LoginRegister/LoginRegister.tsx
+++ app/components/LoginRegister/LoginRegister.tsx
@@ -71,82 +71,84 @@ const LoginRegister: React.FC<LoginRegisterProps> = ({
   return (
     <div className="container my-20 max-w-sm p-5 shadow-loginShadow">
       <h2 className="text-grey-100 text-md font-bold mb-5">{heading}</h2>
-      <div className="flex flex-col gap-4">
-        {isSignUp && (
+      <form onSubmit={handleSubmit}>
+        <div className="flex flex-col gap-4">
+          {isSignUp && (
+            <FTInput
+              Icon={CiUser}
+              type="text"
+              name="name"
+              disabled={loading}
+              placeholder="Name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          )}
           <FTInput
-            Icon={CiUser}
-            type="text"
-            name="name"
+            Icon={CiMail}
+            type="email"
+            name="email"
             disabled={loading}
-            placeholder="Name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
           />
-        )}
-        <FTInput
-          Icon={CiMail}
-          type="email"
-          name="email"
-          disabled={loading}
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <FTInput
-          Icon={CiLock}
-          type="password"
-          name="password"
-          disabled={loading}
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div>
-          <input type="checkbox" id="remember" name="remember" />
-          <label htmlFor="remember" className="ms-2">
-            Remember Me?
-          </label>
-        </div>
-
-        {/* Show spinner or button text based on loading state */}
-        <SolidButton
-          text={
-            loading ? (
-              <div className="mx-auto w-6 h-6 border-2 border-t-4 border-white border-t-brand-500 rounded-full animate-spin"></div>
-            ) : (
-              heading
-            )
-          }
-          className="text-white rounded-0"
-          onClick={handleSubmit}
-        />
-
-        {!isSignUp && (
-          <div className="text-grey-300 flex justify-between text-xs">
-            <Link href="/signup">Sign Up Here!</Link>
-            <Link href="/signup">Forgot Password?</Link>
+          <FTInput
+            Icon={CiLock}
+            type="password"
+            name="password"
+            disabled={loading}
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <div>
+            <input type="checkbox" id="remember" name="remember" />
+            <label htmlFor="remember" className="ms-2">
+              Remember Me?
+            </label>
           </div>
-        )}
 
-        <div className="flex items-center mt-8 mb-4">
-          <hr className="flex-grow border-t" />
-          <span className="p-1 border text-gray-500 helvetica">OR</span>
-          <hr className="flex-grow border-t" />
+          {/* Show spinner or button text based on loading state */}
+          <SolidButton
+            text={
+              loading ? (
+                <div className="mx-auto w-6 h-6 border-2 border-t-4 border-white border-t-brand-500 rounded-full animate-spin"></div>
+              ) : (
+                heading
+              )
+            }
+            className="text-white rounded-0"
+            onClick={handleSubmit}
+          />
+
+          {!isSignUp && (
+            <div className="text-grey-300 flex justify-between text-xs">
+              <Link href="/signup">Sign Up Here!</Link>
+              <Link href="/signup">Forgot Password?</Link>
+            </div>
+          )}
+
+          <div className="flex items-center mt-8 mb-4">
+            <hr className="flex-grow border-t" />
+            <span className="p-1 border text-gray-500 helvetica">OR</span>
+            <hr className="flex-grow border-t" />
+          </div>
+
+          <button
+            className="flex items-center border border-grey-500 px-4 py-2 helvetica text-grey-200"
+            onClick={() => signIn("google")}
+          >
+            <FcGoogle fontSize={20} />
+            <span className="w-full"> Sign Up with Google </span>
+          </button>
+
+          <button className="flex items-center border border-grey-500 px-4 py-2 helvetica text-grey-200">
+            <GrApple fontSize={20} />
+            <span className="w-full"> Sign Up with Apple </span>
+          </button>
         </div>
-
-        <button
-          className="flex items-center border border-grey-500 px-4 py-2 helvetica text-grey-200"
-          onClick={() => signIn("google")}
-        >
-          <FcGoogle fontSize={20} />
-          <span className="w-full"> Sign Up with Google </span>
-        </button>
-
-        <button className="flex items-center border border-grey-500 px-4 py-2 helvetica text-grey-200">
-          <GrApple fontSize={20} />
-          <span className="w-full"> Sign Up with Apple </span>
-        </button>
-      </div>
+      </form>
     </div>
   );
 };
