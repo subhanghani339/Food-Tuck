16267ae16a14a88be85821a49529f27b155f8d92
subhan-skysys <muhammad.subhan@myskysys.com>
2025-02-17 21:02:27 +0500

    layout meta updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,28 @@ export const metadata: Metadata = {
   title: "Foodtuck - Food Restaurant",
   description:
     "Foodtuck is a vibrant food restaurant offering a variety of delicious dishes, from gourmet meals to quick bites. Enjoy exceptional flavors, fresh ingredients, and a welcoming atmosphere for every meal. Visit us today for an unforgettable dining experience!",
-  robots: "noodp",
+  robots: "index, follow",
+  openGraph: {
+    title: "Foodtuck - Food Restaurant",
+    description:
+      "Foodtuck offers a range of delicious dishes with fresh ingredients and a warm dining experience. Whether you're after gourmet meals or a quick bite, Foodtuck has something for every taste!",
+    url: "https://ms-foodtuck.vercel.app/",
+    images: [
+      {
+        url: "/images/home/hero-banner.svg",
+        alt: "Foodtuck - A Food Restaurant",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Foodtuck - Food Restaurant",
+    description:
+      "Foodtuck is a vibrant food restaurant offering delicious dishes with fresh ingredients and a welcoming atmosphere. Perfect for any occasion.",
+    images: ["/images/home/hero-banner.svg"],
+  },
 };
+
 
 export default function RootLayout({
   children,
